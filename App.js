b6218db9a4fc2745aa7357721fe4df39c4feb111
JD-2104 <jdanso019@st.ug.edu.gh>
2024-06-19 02:11:29 +0000

    add

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
 const Stack = createNativeStackNavigator()
 
 const App = () => {
